0776bc0dda1c89a35435ab0d49ea5aaff1f11350
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-20 09:50:53 +0530

    Add files via upload

---
+++ IssueTrackerApp/Controllers/IssueController.cs
@@ -0,0 +1,69 @@
+﻿using IssueTrackerApp.DAL.Interrfaces;
+using IssueTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace IssueTrackerApp.Controllers
+{
+    public class IssueController : ApiController
+    {
+        private readonly IIssueTrackerService _service;
+        public IssueController(IIssueTrackerService service)
+        {
+            _service = service;
+        }
+        public IssueController()
+        {
+            // Constructor logic, if needed
+        }
+        [HttpPost]
+        [Route("api/Issue/CreateIssue")]
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> CreateIssue([FromBody] Issue model)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPut]
+        [Route("api/Issue/UpdateIssue")]
+        public async Task<IHttpActionResult> UpdateIssue([FromBody] Issue model)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpDelete]
+        [Route("api/Issue/DeleteIssue")]
+        public async Task<IHttpActionResult> DeleteIssue(long id)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpGet]
+        [Route("api/Issue/GetIssueById")]
+        public async Task<IHttpActionResult> GetIssueById(long id)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpGet]
+        [Route("api/Issue/GetAllIssues")]
+        public async Task<IEnumerable<Issue>> GetAllIssues()
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IssueTrackerApp/DAL/Services/IssueTrackerService.cs
@@ -0,0 +1,52 @@
+﻿using IssueTrackerApp.DAL.Interrfaces;
+using IssueTrackerApp.DAL.Services.Repository;
+using IssueTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace IssueTrackerApp.DAL.Services
+{
+    public class IssueTrackerService : IIssueTrackerService
+    {
+        private readonly IIssueTrackerRepository _repository;
+
+        public IssueTrackerService(IIssueTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Issue> CreateIssue(Issue issue)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteIssueById(long id)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+        public List<Issue> GetAllIssues()
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+        public Task<Issue> GetIssueById(long id)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+
+        public Task<Issue> UpdateIssue(Issue model)
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+    }
+}
